d2907e2e501508345d02b884b3da83703f6ff7b2
Learner142815 <51354458+Learner142815@users.noreply.github.com>
2024-01-11 15:13:34 +0530

    "second change"

--- mainprogram.py
+++ mainprogram.py
@@ -1,6 +1,6 @@
 import sys
 from config import database_details
-
+from util import get_tables
 def main():
     print("hello world")
     """ this program takes one argument as env variable but in practical you need to write another 
@@ -8,8 +8,9 @@ def main():
     env = sys.argv[1] # check env variables under run option
     db_details = database_details[env]
    # print(db_details)
-
-
+    tables = get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
 
 if __name__=='__main__':
     main()
--- util.py
+++ util.py
@@ -1,3 +1,11 @@
 """
 This file contains connections from source database to destination database
 """
+
+import pandas as pd
+
+
+def get_tables(path):
+    tables = pd.read_csv(path,sep=':')
+    return tables.query('to_be_loaded == "yes"')
+
